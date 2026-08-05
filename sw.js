de9f51b1652879f08d6f506b06467918886a9d49
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2026-08-05 11:01:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'admin-os10-v6';
+const CACHE_NAME = 'admin-os10-v7';
 const ASSETS = [
   './pages/admin.html',
   './pages/modificar.html',
-  './assets/css/style.css?v=3.6',
-  './assets/js/app.js?v=3.6',
+  './assets/css/style.css?v=3.7',
+  './assets/js/app.js?v=3.7',
   './assets/images/logo-os10.webp',
   './assets/images/logo-os.webp',
   './favicon/favicon.ico',
